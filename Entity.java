a86c38e3c64941510aeb23cc11646787c7b2be1b
Kevin Kavaliauskas <kevinkavaliauskas@icloud.com>
2025-02-17 22:52:26 +0000

    refactor: entity

--- Entity.java
+++ Entity.java
@@ -3,29 +3,33 @@ import java.util.Random;
 /**
  * Common elements of foxes and rabbits.
  *
- * @author David J. Barnes and Michael Kölling
+ * @author David J. Barnes and Michael Kölling and Aashwin Eldo and Kevin
+ *         Kavaliauskas
  * @version 7.0
  */
-public abstract class Entity
-{
+public abstract class Entity {
     // Whether the animal is alive or not.
     private boolean alive;
     // The animal's position.
     private Location location;
-    
+
     protected static final Random rand = Randomizer.getRandom();
 
     /**
-     * Constructor for objects of class Animal.
-     * @param location The animal's location.
+     * Constructor for objects of class Entity.
+     * 
+     * @param location The entity's location.
      */
-    public Entity(Location location)
-    {
+    public Entity(Location location) {
         this.alive = true;
         this.location = location;
     }
-    
-    protected void deathToExtremeWeather(String weather){
+
+    /**
+     * 
+     * @param weather The weather of the current step.
+     */
+    protected void deathToExtremeWeather(String weather) {
         if (weather.equals("tornado") || weather.equals("lightning") || weather.equals("snow")) {
             // 10% chance of an entity dying due to extreme weather
             if (Math.random() < 0.1) {
@@ -34,47 +38,49 @@ public abstract class Entity
             }
         }
     }
-    
+
     /**
-     * Act.
-     * @param currentField The current state of the field.
+     * Act method for the entity which is called on every step of the simulation.
+     * 
+     * @param currentField   The current state of the field.
      * @param nextFieldState The new state being built.
+     * @param isDay          Whether it is day or night.
+     * @param weather        The weather of the current step.
      */
     abstract protected void act(Field currentField, Field nextFieldState, Boolean isDay, String weather);
-    
+
     /**
      * Check whether the animal is alive or not.
+     * 
      * @return true if the animal is still alive.
      */
-    public boolean isAlive()
-    {
+    public boolean isAlive() {
         return alive;
     }
 
     /**
      * Indicate that the animal is no longer alive.
      */
-    protected void setDead()
-    {
+    protected void setDead() {
         alive = false;
         location = null;
     }
-    
+
     /**
      * Return the animal's location.
+     * 
      * @return The animal's location.
      */
-    public Location getLocation()
-    {
+    public Location getLocation() {
         return location;
     }
-    
+
     /**
-     * Set the animal's location.
+     * Set the entity's location.
+     * 
      * @param location The new location.
      */
-    protected void setLocation(Location location)
-    {
+    protected void setLocation(Location location) {
         this.location = location;
     }
 }
